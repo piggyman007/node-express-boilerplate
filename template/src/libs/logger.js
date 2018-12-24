4e6b0d29570d85c232d4275d65211d502e541930
ALONGKORN CHETASUMON <draood@gmail.com>
2018-12-24 14:58:22 +0700

    Update logger.js
    update logger lib

--- template/src/libs/logger.js
+++ template/src/libs/logger.js
@@ -1,29 +1,3 @@
 const bunyan = require('bunyan');
 
-module.exports = (config) => {
-  const bunyanConfig = [];
-  const levels = Object.keys(config.levels);
-
-  levels.forEach((level) => {
-    const bunyanLevel = config.levels[level];
-    if (!bunyanLevel) return;
-
-    if (level === 'debug' && config.level !== 'debug') return;
-
-    const logger = { level };
-
-    if (bunyanLevel === 'STDOUT') {
-      logger.stream = process.stdout;
-    } else if (bunyanLevel === 'STDERR') {
-      logger.stream = process.stderr;
-    } else if (bunyanLevel) {
-      logger.path = bunyanLevel;
-    } else {
-      return;
-    }
-
-    bunyanConfig.push(logger);
-  });
-
-  return bunyan.createLogger({ name: config.name, streams: bunyanConfig });
-};
+module.exports = config => bunyan.createLogger({ name: config.name });
