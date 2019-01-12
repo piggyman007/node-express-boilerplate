b225dee027e6d16c13014b1c2d7528be5a914ef4
Alongkorn Chetasumon <draood@gmail.com>
2019-01-13 00:41:07 +0700

    clean code

--- src/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  port: process.env.PORT || 3000,
-  shutdownTimeout: process.env.SHUTDOWN_TIMEOUT || 1000,
-  logger: {
-    name: 'sign-ap-is',
-    level: process.env.LOG_LEVEL || 'info'
-  }
-};
--- src/libs/logger.js
+++
@@ -1,3 +0,0 @@
-const bunyan = require('bunyan');
-
-module.exports = config => bunyan.createLogger({ name: config.name });
